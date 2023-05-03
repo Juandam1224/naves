21083d78e9424fdb2975a8f7864c330119069cf9
Juandam1224 <juandavidmarintoro@pascualbravo.edu.co>
2023-05-03 03:36:49 -0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.tmrFiuu = new System.Windows.Forms.Timer(this.components);
+            this.imgPlayer = new System.Windows.Forms.PictureBox();
+            this.lblMuroI = new System.Windows.Forms.Label();
+            this.lblMuroD = new System.Windows.Forms.Label();
+            this.imgEnemy = new System.Windows.Forms.PictureBox();
+            this.imgDisparo = new System.Windows.Forms.PictureBox();
+            this.tmrDisparo = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgEnemy)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgDisparo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 415);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(708, 415);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(75, 23);
+            this.btnStop.TabIndex = 2;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // tmrFiuu
+            // 
+            this.tmrFiuu.Tick += new System.EventHandler(this.tmrFiuu_Tick);
+            // 
+            // imgPlayer
+            // 
+            this.imgPlayer.BackColor = System.Drawing.Color.Transparent;
+            this.imgPlayer.Image = ((System.Drawing.Image)(resources.GetObject("imgPlayer.Image")));
+            this.imgPlayer.Location = new System.Drawing.Point(344, 380);
+            this.imgPlayer.Name = "imgPlayer";
+            this.imgPlayer.Size = new System.Drawing.Size(57, 32);
+            this.imgPlayer.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imgPlayer.TabIndex = 5;
+            this.imgPlayer.TabStop = false;
+            // 
+            // lblMuroI
+            // 
+            this.lblMuroI.AutoSize = true;
+            this.lblMuroI.ForeColor = System.Drawing.Color.Silver;
+            this.lblMuroI.Location = new System.Drawing.Point(774, 399);
+            this.lblMuroI.Name = "lblMuroI";
+            this.lblMuroI.Size = new System.Drawing.Size(9, 13);
+            this.lblMuroI.TabIndex = 6;
+            this.lblMuroI.Text = "|";
+            this.lblMuroI.Visible = false;
+            // 
+            // lblMuroD
+            // 
+            this.lblMuroD.AutoSize = true;
+            this.lblMuroD.ForeColor = System.Drawing.Color.Silver;
+            this.lblMuroD.Location = new System.Drawing.Point(12, 399);
+            this.lblMuroD.Name = "lblMuroD";
+            this.lblMuroD.Size = new System.Drawing.Size(9, 13);
+            this.lblMuroD.TabIndex = 7;
+            this.lblMuroD.Text = "|";
+            this.lblMuroD.Visible = false;
+            // 
+            // imgEnemy
+            // 
+            this.imgEnemy.BackColor = System.Drawing.Color.Transparent;
+            this.imgEnemy.Image = ((System.Drawing.Image)(resources.GetObject("imgEnemy.Image")));
+            this.imgEnemy.Location = new System.Drawing.Point(344, 2);
+            this.imgEnemy.Name = "imgEnemy";
+            this.imgEnemy.Size = new System.Drawing.Size(36, 31);
+            this.imgEnemy.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imgEnemy.TabIndex = 8;
+            this.imgEnemy.TabStop = false;
+            // 
+            // imgDisparo
+            // 
+            this.imgDisparo.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.imgDisparo.Location = new System.Drawing.Point(375, 364);
+            this.imgDisparo.Name = "imgDisparo";
+            this.imgDisparo.Size = new System.Drawing.Size(5, 10);
+            this.imgDisparo.TabIndex = 9;
+            this.imgDisparo.TabStop = false;
+            // 
+            // tmrDisparo
+            // 
+            this.tmrDisparo.Tick += new System.EventHandler(this.tmrDisparo_Tick);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(795, 450);
+            this.Controls.Add(this.imgDisparo);
+            this.Controls.Add(this.imgEnemy);
+            this.Controls.Add(this.lblMuroD);
+            this.Controls.Add(this.lblMuroI);
+            this.Controls.Add(this.imgPlayer);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "frmMain";
+            this.Text = "Main form";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmMain_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.frmMain_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.imgPlayer)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgEnemy)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imgDisparo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Timer tmrFiuu;
+        private System.Windows.Forms.PictureBox imgPlayer;
+        private System.Windows.Forms.Label lblMuroI;
+        private System.Windows.Forms.Label lblMuroD;
+        private System.Windows.Forms.PictureBox imgEnemy;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox imgDisparo;
+        private System.Windows.Forms.Timer tmrDisparo;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class frmMain : Form
+    {
+        int imgEnemyX, imgEnemyY, enemysVelocityH = 40, enemysVelocityV = 4, playersVelocityWithArrows = 20, playersVelocityWithLetters = 7;
+        bool tocaMuroD = false, tocaMuroI = false, llegaAbajo = false, llegaArriba = true;
+
+        
+
+        int imgEnemyPosInitX = 0, imgEnemyPosInitY = 0;
+
+        public frmMain()
+        {
+            InitializeComponent();
+            KeyPreview = true;
+            imgEnemyPosInitX = imgEnemy.Location.X;
+            imgEnemyPosInitY = imgEnemy.Location.Y;
+
+            imgDisparo.Location = new Point(imgPlayer.Location.X + (imgPlayer.Width / 2), imgPlayer.Location.Y + 5);
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrFiuu.Enabled = true;
+            tmrDisparo.Enabled = true;
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            tmrFiuu.Enabled = false;
+            tmrDisparo.Enabled = false;
+
+        }
+
+        private void tmrFiuu_Tick(object sender, EventArgs e)
+        {
+            
+            //obtenemos la posicion del enemy (el rayo mcqueen)
+            imgEnemyX = imgEnemy.Location.X;
+            imgEnemyY = imgEnemy.Location.Y;
+
+            //Estos ifs sirven para que no se salga de la pantalla ni por arriba ni por abajo
+            //verifica que el enemy nunca se pase por afuera de la ventana por encima
+            if (imgEnemy.Location.Y <= 0)
+            {
+                llegaArriba = true;
+                llegaAbajo = false;
+            }
+            //verifica que el enemy no llegue a un height mas alto que el de la ventana
+            if (imgEnemy.Location.Y >= (Size.Height - (imgEnemy.Size.Height * 2)))
+            {
+                llegaAbajo = true;
+                llegaArriba = false;
+            }
+
+            //verificamos que toque un muro y que no esté por afuera de la pantalla por arriba y lo ponemos a bajar
+            if (((tocaMuroD == true || tocaMuroI == true) && (llegaAbajo == false)) || llegaArriba == true)
+            {
+                llegaAbajo = false;
+                imgEnemy.Location = new Point(imgEnemyX, imgEnemyY += enemysVelocityV);
+            }
+
+            //verificamos que toque un muro y que no esté por afuera de la pantalla por abajo y lo ponemos a subir
+            if (((tocaMuroD == true || tocaMuroI == true) && (llegaArriba == false)) || llegaAbajo == true)
+            {
+                llegaArriba = false;
+                imgEnemy.Location = new Point(imgEnemyX, imgEnemyY -= enemysVelocityV);
+            }
+
+
+
+            //apenas toque un muro vaya y toque el otro
+            if (tocaMuroD == false || tocaMuroI == true)
+            {
+                imgEnemy.Location = new Point(imgEnemyX + enemysVelocityH, imgEnemyY);
+                //if (imgMcQueen.Location.X >= (lblMuroD.Location.X - imgMcQueen.Size.Width))
+                if (imgEnemy.Location.X >= (Size.Width - imgEnemy.Size.Width))
+                {
+                    tocaMuroD = true;
+                    tocaMuroI = false;
+
+                }
+            }
+            if (tocaMuroD == true || tocaMuroI == false)
+            {
+                imgEnemy.Location = new Point(imgEnemyX - enemysVelocityH, imgEnemyY);
+                //if (imgMcQueen.Location.X < lblMuroI.Location.X)
+                if (imgEnemy.Location.X <= (0 + (imgEnemy.Size.Width / 2)))
+                {
+                    tocaMuroI = true;
+                    tocaMuroD = false;
+
+                }
+            }
+
+
+            if(imgEnemy.Bounds.IntersectsWith(imgPlayer.Bounds))
+            {
+                tmrFiuu.Enabled = false;
+                tmrDisparo.Enabled = false;
+                MessageBox.Show("Perdiste");
+                imgEnemy.Location = new Point(imgEnemyPosInitX, imgEnemyPosInitY);
+                imgDisparo.Location = new Point(imgPlayer.Location.X + (imgPlayer.Width / 2), imgPlayer.Location.Y + 5);
+            }
+
+            if (imgEnemy.Bounds.IntersectsWith(imgDisparo.Bounds))
+            {
+                tmrFiuu.Enabled = false;
+                tmrDisparo.Enabled = false;
+                MessageBox.Show("Ganaste");
+                imgEnemy.Location = new Point(imgEnemyPosInitX, imgEnemyPosInitY);
+                imgDisparo.Location = new Point(imgPlayer.Location.X + (imgPlayer.Width / 2), imgPlayer.Location.Y + 5);
+            }
+
+
+        }
+
+        private void tmrDisparo_Tick(object sender, EventArgs e)
+        {
+            while(imgDisparo.Location.Y > 0 && tmrFiuu.Enabled == true)
+            {
+                //a
+                imgDisparo.Location = new Point(imgDisparo.Location.X, imgDisparo.Location.Y - 5);
+                Application.DoEvents(); // Permite actualizar la ventana
+                Thread.Sleep(20); // Retrasar el hilo durante 20 milisegundos
+            }
+
+            if (imgDisparo.Location.Y >= 0)
+            {
+                imgDisparo.Location = new Point(imgPlayer.Location.X + (imgPlayer.Width / 2), imgPlayer.Location.Y + 5);
+            }
+            
+        }
+
+        
+        //Puede moverse de dos maneras
+
+        //1-Spameando A o D pero cada paso es mas lento que usando las flechas
+        private void frmMain_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (imgPlayer.Location.X >= btnStart.Location.X)
+            {
+                if (e.KeyChar.ToString().ToUpper() == Convert.ToChar(Keys.A).ToString().ToUpper())
+                {
+                    imgPlayer.Location = new Point(imgPlayer.Location.X - playersVelocityWithLetters, imgPlayer.Location.Y);
+                }
+            }
+            
+            if(imgPlayer.Location.X <= (btnStop.Location.X + (btnStop.Size.Width * 0.2) ))
+            {
+                if (e.KeyChar.ToString().ToUpper() == Convert.ToChar(Keys.D).ToString().ToUpper())
+                {
+
+                    imgPlayer.Location = new Point(imgPlayer.Location.X + playersVelocityWithLetters, imgPlayer.Location.Y);
+                }
+            }
+        }
+
+        //2- Con las teclas no se puede spamear pero cada paso es mas largo que usando A o D
+        private void frmMain_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (imgPlayer.Location.X >= (0 + (imgPlayer.Width * 0.5)))
+            {
+                if (e.KeyCode == Keys.Left)
+                {
+                    imgPlayer.Location = new Point(imgPlayer.Location.X - playersVelocityWithArrows, imgPlayer.Location.Y);
+                }
+            }
+
+            if (imgPlayer.Location.X <= (Size.Width - (imgPlayer.Width * 2)))
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    imgPlayer.Location = new Point(imgPlayer.Location.X + playersVelocityWithArrows, imgPlayer.Location.Y);
+                }
+            }
+
+
+            if(e.KeyCode == Keys.Space)
+            {
+               
+
+
+
+                
+               
+                
+
+               
+            }
+
+            
+
+        }
+
+    }
+}
+
+
